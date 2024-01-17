dea2a97085b4f479642be0c60757fbb7d64cb916
Hekiku <darkness.within.2003@gmail.com>
2024-01-17 12:39:33 +0800

    kelas

--- kelas.php
+++ kelas.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+include 'inc/database-inc.php';
+
+$sql = "SELECT * FROM kelas";
+$hasil = mysqli_query($conn, $sql);
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -9,14 +16,12 @@
   <body>
     <h1 id="header">Sistem Kehadiran Cavell</h1>
     <ul id="menu">
-      <li><a href="index.html">Halaman Utama</a></li>
-      <li><a href="senarai_aktiviti.html">Senarai Aktiviti</a></li>
-      <li><a href="log_masuk.html">Log Masuk</a></li>
+      <?php include 'inc/menu.php' ?>
     </ul>
     <h2 id="tajuk">Kelas</h2>
-    <form id="borang" action="" method="post">
+    <form id="borang" action="inc/kelas_tambah-inc.php" method="post">
       <label for="tingkatan">Tingkatan</label>
-      <select name="tingkatan" id="tingkatan">
+      <select name="tingkatan" id="tingkatan" required>
         <option value="" selected></option>
         <option value="1">Satu</option>
         <option value="2">Dua</option>
@@ -25,7 +30,7 @@
         <option value="5">Lima</option>
       </select>
       <label for="namaKelas">Nama Kelas</label>
-      <input type="text" name="namaKelas" id="namaKelas" />
+      <input type="text" name="namaKelas" id="namaKelas" required />
       <button type="submit" name="tambah">Tambah</button>
     </form>
     <table id="senarai">
@@ -35,26 +40,23 @@
         <th>Nama Kelas</th>
         <th>Hapus</th>
       </tr>
+      <?php
+      $bil = 0;
+      while ($rekod = mysqli_fetch_assoc($hasil)) {
+        $bil++;  
+        $idKelas = $rekod['idKelas'];
+        $tingkatan = $rekod['tingkatan'];
+        $namaKelas = $rekod['namaKelas'];
+      ?>
       <tr>
-        <td>1.</td>
-        <td>1</td>
-        <td>Akik</td>
-        <td><a href="">Hapus</a></td>
+        <td><?php echo $bil ?>.</td>
+        <td><?php echo $tingkatan ?></td>
+        <td><?php echo $namaKelas ?></td>
+        <td><a href="inc/kelas_hapus-inc.php?idKelas=<?php echo $idKelas ?>">Hapus</a></td>
       </tr>
-      <tr>
-        <td>2.</td>
-        <td>1</td>
-        <td>Delima</td>
-        <td><a href="">Hapus</a></td>
-      </tr>
-      <tr>
-        <td>3.</td>
-        <td>2</td>
-        <td>Intan</td>
-        <td><a href="">Hapus</a></td>
-      </tr>
-    </table>
-    <p>Muat naik ahli. Klik <a href="muat_naik.html">di sini</a></p>
-    <p>Tambah kelas <a href="kelas.html">di sini</a></p>
+      <?php
+      }
+      ?>
+   </table>
   </body>
 </html>
