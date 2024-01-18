20969aa41f65f5598353e58484bb8030a12dc1e8
Hekiku <darkness.within.2003@gmail.com>
2024-01-18 11:09:51 +0800

    muat naik ahli

--- muat_naik.php
+++ muat_naik.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -9,13 +12,11 @@
   <body>
     <h1 id="header">Sistem Kehadiran Cavell</h1>
     <ul id="menu">
-      <li><a href="index.html">Halaman Utama</a></li>
-      <li><a href="senarai_aktiviti.html">Senarai Aktiviti</a></li>
-      <li><a href="log_masuk.html">Log Masuk</a></li>
+        <?php include 'inc/menu.php' ?>
     </ul>
-    <form id="borang" action="" method="post">
+    <form id="borang" action="inc/muat_naik-inc.php" method="post" enctype="multipart/form-data">
       <h2 id="tajuk">Muat Naik Ahli</h2>
-      <input type="file" name="ahli" id="ahli" />
+      <input type="file" name="senaraiAhli" id="senaraiAhli" />
       <button type="submit" name="muat_naik">Muat Naik</button>
     </form>
   </body>
